a059195593c5b4043a1949a5837d30d47a830b2f
KADDARI Ziad <ziadkaddari1@gmail.com>
2024-08-19 13:36:21 +0100

    Controller

---
+++ CI_CD Pipelines/Controllers/TestController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CI_CD_Pipelines.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class TestController : ControllerBase
+{
+    
+    [HttpGet("int")]
+    public ActionResult<int> IntFunc()
+    {
+        return Ok(13);
+    }
+    [HttpGet("string")]
+    public ActionResult<string> StringFunc()
+    {
+        return Ok("Ziad");
+    }
+}
